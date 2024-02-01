ca638046eb6ef67943b1f8275f2ed083525f31d2
GayeBuse <gayebuseozkan@gmail.com>
2024-02-01 22:57:43 +0300

    first commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,28 @@
+import ModeSwitch from "./components/ModeSwitch/ModeSwitch";
+import Header from "./components/Header/Header";
+import Hero from "./components/Hero/Hero";
+import Skills from "./components/Skills/Skills";
+import Profile from "./components/Profile/Profile";
+import Projects from "./components/Projects/Projects";
+import Footer from "./components/Footer/Footer";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <div className="min-container">
+          <ModeSwitch />
+          <Header />
+          <Hero />
+          <Skills />
+          <Profile />
+          <Projects />
+        </div>
+        <Footer />
+      </div>
+    </>
+  );
+}
+
+export default App;
