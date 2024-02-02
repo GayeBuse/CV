20b67cc4e448c6060229dc83738d21c2643e3c8d
GayeBuse <gayebuseozkan@gmail.com>
2024-02-02 16:21:23 +0300

    setup

--- vite-project/src/components/ModeSwitch/ModeSwitch.jsx
+++ vite-project/src/components/ModeSwitch/ModeSwitch.jsx
@@ -1,3 +1,14 @@
+import "./ModeSwitch.css";
 export default function ModeSwitch() {
-  return <div className="container">ModeSwitch Component</div>;
+  return (
+    <div className="container">
+      <div className="min-container">
+        <div className="dark-mode">DARK MODE | </div>
+        <div>
+          <span className="dil">TÜRKÇE</span>
+          <span className="dil">'YE GEÇ</span>
+        </div>
+      </div>
+    </div>
+  );
 }
