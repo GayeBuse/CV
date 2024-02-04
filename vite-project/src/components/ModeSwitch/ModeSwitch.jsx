e980adcfee8defe4d14fdc264ca83df92352a123
GayeBuse <gayebuseozkan@gmail.com>
2024-02-04 21:50:58 +0300

    dark mode aktive edildi

--- vite-project/src/components/ModeSwitch/ModeSwitch.jsx
+++ vite-project/src/components/ModeSwitch/ModeSwitch.jsx
@@ -1,9 +1,22 @@
 import { IoToggleSharp } from "react-icons/io5";
+import { useContext, useState } from "react";
+import { DarkModeContext } from "../../context/DarkMode";
 import "./ModeSwitch.css";
 export default function ModeSwitch() {
+  const { theme, toggleTheme } = useContext(DarkModeContext);
+  const [localTheme, setLocalTheme] = useState(
+    localStorage.getItem("theme") || theme
+  ); /*tarayıcının yerel depolama alanından bir tema değeri almak ve bu değeri localTheme state'ine atamaktır. Eğer böyle bir değer yoksa veya geçersizse, varsayılan olarak theme adında başka bir değeri kullanmaktır*/
+  const handleThemeChange = () => {
+    setLocalTheme(theme === "light" ? "dark" : "light");
+    toggleTheme();
+    localStorage.setItem("theme", theme); //hangi adla depolanacak
+  };
   return (
     <div className="mode-container">
-      <IoToggleSharp />
+      <button onClick={handleThemeChange}>
+        <IoToggleSharp />
+      </button>
       <span className="dark-mode">DARK MODE | </span>
       <span className="dil">TÜRKÇE</span>
       <span className="as">'YE GEÇ</span>
---
+++ vite-project/src/context/DarkMode.jsx
@@ -0,0 +1,37 @@
+import React, { createContext, useState, useEffect } from "react";
+
+// DarkMode context'i oluşturuluyor
+export const DarkModeContext = createContext();
+
+export const DarkModeContextProvider = ({ children }) => {
+  const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
+
+  // Temayı değiştirme fonksiyonu/tetiklendiğinde bana bunu yap
+  const toggleTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
+  useEffect(() => {
+    // Temayı localStorage'a kaydet
+    localStorage.setItem("theme", theme);
+
+    // Sayfanın temasını değiştir
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+    /*Eğer theme değişkeninin değeri "dark" ise, sayfa "dark" sınıfına sahip olur ve geçerli tema bu sınıfı kullanarak uygulanır. Aksi halde, "dark" sınıfı kaldırılarak sayfa teması normal duruma getirilir.*/
+    // Bileşen yok olduğunda temayı temizle
+    return () => {
+      localStorage.removeItem("theme");
+      document.documentElement.classList.remove("dark");
+    };
+  }, [theme]);
+  //sayfa renderlanmıypr dark olmuyor
+  // DarkModeProvider bileşeni oluşturuluyor
+  return (
+    <DarkModeContext.Provider value={{ theme, setTheme, toggleTheme }}>
+      {children}
+    </DarkModeContext.Provider>
+  );
+};
