f8cd7385c886f28d99d31765bf5e785008d37896
GayeBuse <gayebuseozkan@gmail.com>
2024-02-04 16:26:00 +0300

    react-scroll eklendi

--- vite-project/src/components/Header/Header.jsx
+++ vite-project/src/components/Header/Header.jsx
@@ -1,16 +1,20 @@
 import "./Header.css";
-import { Link } from "react-router-dom";
 
+import { Link, animateScroll as scroll } from "react-scroll";
 export default function Header() {
   return (
     <div className="header-container">
       <nav>
         <ul className="nav-links">
           <li className="nav-item1">
-            <Link to="/Skills">Skills</Link>
+            <Link to="skills" smooth={true}>
+              Skills
+            </Link>
           </li>
           <li className="nav-item2">
-            <Link to="/Projects">Projects</Link>
+            <Link to="projects" smooth={true} duration={3000}>
+              Projects
+            </Link>
           </li>
           <li className="nav-item3">
             <Link to="/HireMe">Hire me</Link>
